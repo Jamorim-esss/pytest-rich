805d1d06e67afe8130fee34ecfccd5bc7bfc67be
jamorim <jamorim@esss.com>
2026-04-08 07:30:25 -0300

    Importing assert never from typing now that its python 3.11

--- src/pytest_rich/terminal.py
+++ src/pytest_rich/terminal.py
@@ -18,7 +18,7 @@ from rich.progress import TaskID
 from rich.rule import Rule
 from rich.table import Table
 from rich.text import Text
-from typing_extensions import assert_never
+from typing import assert_never
 
 from pytest_rich.capture import save_terminal_output
 from pytest_rich.header import generate_header_panel
